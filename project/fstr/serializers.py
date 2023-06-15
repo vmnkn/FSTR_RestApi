85828f51e58556b61cea53324bbbdc2064cedb6b
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-06-15 17:47:20 +0300

    changed serializers and views

--- project/fstr/serializers.py
+++ project/fstr/serializers.py
@@ -6,78 +6,39 @@ from django.contrib.auth.models import User
 class CoordinatesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coordinates
-        fields = ('latitude',
-                  'longitude',
-                  'height', )
+        fields = '__all__'
         verbose_name = 'Координаты'
 
 
 class LevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Level
-        fields = ('winter',
-                  'summer',
-                  'autumn',
-                  'spring', )
+        fields = '__all__'
         verbose_name = 'Уровень сложности'
 
 
-class UserSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='first_name')
-    fam = serializers.CharField(source='last_name')
-    otc = serializers.CharField(source='patronymic')
-    email = serializers.CharField()
-    phone = serializers.CharField()
-
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('email',
-                  'fam',
-                  'name',
-                  'otc',
-                  'phone', )
+        model = Users
+        fields = '__all__'
         verbose_name = 'Пользователь'
 
 
 class ImagesSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
-
     class Meta:
         model = Images
-        fields = ('image',
-                  'title', )
+        fields = '__all__'
         verbose_name = 'Изображение'
 
 
 class PerevalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pereval
-        fields = ('beauty_title',
-                  'title',
-                  'other_titles',
-                  'connect', )
+        fields = '__all__'
 
 
-class PerevalAddSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    perevals = PerevalSerializer()
-    coordinates = CoordinatesSerializer()
-    level = LevelSerializer()
-    image = ImagesSerializer(many=True)
-
+class PerevalUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pereval
-        fields = ('id',
-                  'user',
-                  'perevals',
-                  'coordinates',
-                  'level',
-                  'image',
-                  'status', )
-
-    def create(self, validated_data):
-        image_data = validated_data.pop('image')
-        pereval = Pereval.objects.create(**validated_data)
-        for image in image_data:
-            Images.objects.create(pereval=pereval, **image)
-        return pereval
+        fields = '__all__'
+        read_only_fields = ['user', ]
--- project/fstr/views.py
+++ project/fstr/views.py
@@ -8,19 +8,14 @@ from .serializers import *
 
 class PerevalViewSet(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
-    serializer_class = PerevalAddSerializer
-
-    def create(self, request, *args, **kwargs):
-        image = ImagesSerializer(data=request.data)
-        if image.is_valid():
-            image.save()
-            return Response(status=201)
-        else:
-            print('Ошибка')
-            return Response({'error': 'validation error'}, status=400)
+    serializer_class = PerevalSerializer
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
 
+
+class PerevalList(ListAPIView):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
