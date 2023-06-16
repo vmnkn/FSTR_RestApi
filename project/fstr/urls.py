1b13d4f569a2d49c3a4d66e36e0c4f0692fc9ef8
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-06-16 03:58:29 +0300

    created url for get method

--- project/fstr/urls.py
+++ project/fstr/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PerevalList, submit_data
+from .views import PerevalList, submit_data, get_data
 
 urlpatterns = [
     path('submitData/', PerevalList.as_view(), name='pereval_list'),
     path('submitData/create/', submit_data, name='submit_data'),
+    path('submitData/<int:pk>/', get_data, name='get_data'),
 ]
