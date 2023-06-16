4318f83af5bbfec2102f5cf64c7af83bcb2646da
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-06-16 17:06:26 +0300

    changed settings file for filter

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
+import django_filters.rest_framework
 from dotenv import load_dotenv
 import os
 
@@ -43,8 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fstr.apps.FstrConfig',
+    'fstr',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
