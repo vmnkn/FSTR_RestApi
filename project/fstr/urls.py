2ed1139a35e40146d54fbe9cf73a8907058f8e85
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-06-16 17:19:19 +0300

    released ulr path for PATCH method

--- project/fstr/urls.py
+++ project/fstr/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PerevalList, submit_data, get_data
+from .views import PerevalList, submit_data, get_data, update_data
 
 urlpatterns = [
     path('submitData/', PerevalList.as_view(), name='pereval_list'),
     path('submitData/create/', submit_data, name='submit_data'),
     path('submitData/<int:pk>/', get_data, name='get_data'),
+    path('submitData/<int:pk>/update/', update_data, name='update_data'),
 ]
